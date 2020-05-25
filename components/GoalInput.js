15b2dbe886077340c285799686ea242d85f14bdf
GUPTA Aman <M26@dsone.3ds.com>
2020-05-25 15:53:00 +0530

    css on button can not apllied directory. Button should be in view and apply css to view

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,36 +1,40 @@
-import React,{useState} from 'react';
-import {View, TextInput, StyleSheet, Button, Modal} from 'react-native';
+import React, { useState } from 'react';
+import { View, TextInput, StyleSheet, Button, Modal } from 'react-native';
 
 
 
-const GoalInput = props =>{
+const GoalInput = props => {
     const [enteredGoal, setEnteredGoal] = useState('');
     const goalInputHandler = (enteredText) => {
         setEnteredGoal(enteredText);
-      };
-    const addGoalHandler =() =>{
-          props.onAddGoal(enteredGoal);
-          setEnteredGoal('');
+    };
+    const addGoalHandler = () => {
+        props.onAddGoal(enteredGoal);
+        setEnteredGoal('');
     };
     return (
         <Modal visible={props.visible} animationType="slide">
-        <View style={goalInputStyle.inputContainer}>
-        <TextInput
-          placeholder='add new goal'
-          style={goalInputStyle.input}
-          onChangeText={goalInputHandler}
-          value={enteredGoal}
-        />
-        {/* 
+            <View style={goalInputStyle.inputContainer}>
+                <TextInput
+                    placeholder='add new goal'
+                    style={goalInputStyle.input}
+                    onChangeText={goalInputHandler}
+                    value={enteredGoal}
+                />
+                {/* 
         use arrow function or use bind function
         <Button title="Add" onPress={()=>
             props.onAddGoal(enteredGoal)} /> */}
-            <View style={goalInputStyle.buttonViewStyles}>
-            <Button title="Add" onPress={addGoalHandler} style={goalInputStyle.addButtonStyles}/>
-            <Button title="Cancel" color="red" onPress={props.onCancel}/>
+                <View style={goalInputStyle.buttonViewStyles}>
+                    <View style={goalInputStyle.buttonStyles}>
+                        <Button title="Cancel" color="red" onPress={props.onCancel} />
+                    </View>
+                    <View style={goalInputStyle.buttonStyles}>
+                        <Button title="Add" onPress={addGoalHandler} />
+                    </View>
+                </View>
             </View>
-      </View>
-      </Modal>
+        </Modal>
     );
 };
 
@@ -39,26 +43,25 @@ const goalInputStyle = StyleSheet.create({
     input: {
         borderColor: 'black',
         borderWidth: 1,
-        width: 150,
+        width: '80%',
         padding: 10,
-        marginBottom:5
-      },
+        marginBottom: 10
+    },
     inputContainer: {
         //flexDirection: 'row',
         //justifyContent: 'space-between',
         alignItems: 'center',
-        flex:1,
-        justifyContent:'center'
-      },
-      buttonViewStyles :{
-          flexDirection:"row",
-          margin:10
-      },
-      addButtonStyles:{
-          //this style is not applyin on button
-          padding:5,
-          marginBottom:100
-      }
+        flex: 1,
+        justifyContent: 'center'
+    },
+    buttonViewStyles: {
+        flexDirection: "row",
+        justifyContent: 'space-between',
+        width: '60%'
+    },
+    buttonStyles: {
+        width: '40%'
+    }
 });
 
 
